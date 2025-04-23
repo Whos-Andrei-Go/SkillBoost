23afc060371d14d5a37d313bb1c4213d841d5592
Kwesten <christineanndejito21@gmail.com>
2025-04-23 15:06:17 +0800

    added more things for homepage

---
+++ app/src/main/java/ph/edu/usc/skillboost/CourseAdapter.java
@@ -0,0 +1,54 @@
+package ph.edu.usc.skillboost;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
+
+    private List<Course> courseList;
+
+    public CourseAdapter(List<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+    @NonNull
+    @Override
+    public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_course, parent, false);
+        return new CourseViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+        Course course = courseList.get(position);
+        holder.title.setText(course.title);
+        holder.description.setText(course.description);
+        holder.image.setImageResource(course.imageResId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return courseList.size();
+    }
+
+    static class CourseViewHolder extends RecyclerView.ViewHolder {
+        TextView title, description;
+        ImageView image;
+
+        public CourseViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.text_course_title);
+            description = itemView.findViewById(R.id.text_course_desc);
+            image = itemView.findViewById(R.id.image_course);
+        }
+    }
+}
+
