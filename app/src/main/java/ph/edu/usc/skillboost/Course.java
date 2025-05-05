9021b86f207b17bffc2c68e9bfb9ef586755620c
user <mykroble@gmail.com>
2025-05-05 08:57:03 +0800

    edited ProfileActivity.java, activity_profile.xml, and Course + added item_course_completed.xml, rounded_profile_image.xml

--- app/src/main/java/ph/edu/usc/skillboost/Course.java
+++ app/src/main/java/ph/edu/usc/skillboost/Course.java
@@ -4,11 +4,17 @@ public class Course {
     int imageResId;
     String title;
     String description;
+    private boolean isCompleted; // completed in user profile
 
-    public Course(int imageResId, String title, String description) {
+    public Course(int imageResId, String title, String description, boolean isCompleted) {
         this.imageResId = imageResId;
         this.title = title;
         this.description = description;
+        this.isCompleted = isCompleted;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
     }
 }
 
--- app/src/main/java/ph/edu/usc/skillboost/ProfileActivity.java
+++ app/src/main/java/ph/edu/usc/skillboost/ProfileActivity.java
@@ -1,12 +1,43 @@
 package ph.edu.usc.skillboost;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileActivity extends BaseActivity {
+
+    LinearLayout moreCourses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentLayout(R.layout.activity_profile);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_courses_completed);
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course(R.drawable.course1, "Math Basics", "Introduction to Math", true));
+        courseList.add(new Course(R.drawable.course2, "Advanced Java", "Deep dive into OOP", true));
+        courseList.add(new Course(R.drawable.course1, "UI/UX Design", "Design modern interfaces", true));
+
+        CourseAdapter adapter = new CourseAdapter(courseList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setAdapter(adapter);
+
+        moreCourses = findViewById(R.id.more_courses_completed);
+
+        moreCourses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, CoursesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
